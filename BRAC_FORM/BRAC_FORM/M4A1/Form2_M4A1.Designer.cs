4bc93211b50ec90403ecbd17b0170db74d31e5f7
Petersén Tim <tim.petersen2@saabgroup.com>
2025-04-09 11:59:43 +0200

    HEJJEHEJ

--- BRAC_FORM/BRAC_FORM/M4A1/Form2_M4A1.Designer.cs
+++ BRAC_FORM/BRAC_FORM/M4A1/Form2_M4A1.Designer.cs
@@ -37,12 +37,17 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button4
             // 
             this.button4.Location = new System.Drawing.Point(332, 391);
-            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(135, 44);
             this.button4.TabIndex = 18;
@@ -53,7 +58,7 @@
             // button3
             // 
             this.button3.Location = new System.Drawing.Point(260, 276);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(133, 44);
             this.button3.TabIndex = 17;
@@ -64,7 +69,7 @@
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(508, 407);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 28);
             this.button2.TabIndex = 16;
@@ -75,7 +80,7 @@
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(25, 407);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 15;
@@ -106,7 +111,7 @@
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(260, 117);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(132, 22);
             this.textBox2.TabIndex = 12;
@@ -115,7 +120,7 @@
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(260, 60);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(132, 22);
             this.textBox1.TabIndex = 11;
@@ -131,11 +136,65 @@
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(161, 391);
+            this.button6.Margin = new System.Windows.Forms.Padding(4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(135, 44);
+            this.button6.TabIndex = 20;
+            this.button6.Text = "Update";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(260, 164);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(132, 22);
+            this.textBox3.TabIndex = 21;
+            this.textBox3.Text = "40";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(260, 215);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(132, 22);
+            this.textBox4.TabIndex = 22;
+            this.textBox4.Text = "10";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(141, 167);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 16);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Fork Length";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(147, 215);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 16);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Fork Width";
+            // 
             // Form2_M4A1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(632, 496);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -145,7 +204,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form2_M4A1";
             this.Text = "Form2_M4A1";
             this.ResumeLayout(false);
@@ -164,5 +223,10 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
--- BRAC_FORM/BRAC_FORM/M4A1/Form2_M4A1.cs
+++ BRAC_FORM/BRAC_FORM/M4A1/Form2_M4A1.cs
@@ -10,15 +10,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace BRAC_FORM
 {
     public partial class Form2_M4A1: Form
     {
+        string originalTextbox1 = "";
+        string originalTextbox2 = "";
+        string originalTextbox3 = "";
+        string originalTextbox4 = "";
         public Form2_M4A1()
         {
             InitializeComponent();
             button4.Enabled = false;
+            button6.Enabled = false;
         }
 
         private void button3_Click(object sender, EventArgs e) ////////////////// ADD BRACKET
@@ -51,9 +57,9 @@ namespace BRAC_FORM
             int FrontPOS = bracketPos - bracketWidth / 2 - FrontDif + 9;
 
 
-            //string Gaffel_L = FrontPOS.ToString();
-            string Gaffel_L = "10";
-            string Gaffel_W = "10";
+           
+            string Gaffel_L = textBox3.Text;
+            string Gaffel_W = textBox4.Text;
 
 
             //GlobalVariables.FilePath = GlobalVariables.FilePath + "\\Nya_CAD";
@@ -77,9 +83,12 @@ namespace BRAC_FORM
 
             //GlobalVariables.FilePath = GlobalVariables.FilePath.Replace("\\Nya_CAD", "");
 
+            originalTextbox1 = textBox1.Text;
+            originalTextbox2 = textBox2.Text;
 
             button4.Enabled = true;
             button3.Enabled = false;
+            button6.Enabled = true;
         }
 
         private void button4_Click(object sender, EventArgs e) ///////////////////////// DELETE
@@ -88,7 +97,7 @@ namespace BRAC_FORM
             
             //GlobalVariables.FilePath = GlobalVariables.FilePath + "\\Nya_CAD";
 
-            UI.GetUI().NXMessageBox.Show("Error", NXMessageBox.DialogType.Error, $"{GlobalVariables.FilePath}");
+            //UI.GetUI().NXMessageBox.Show("Error", NXMessageBox.DialogType.Error, $"{GlobalVariables.FilePath}");
 
             addItem.DeleteBracket("Locking_brack", GlobalVariables.bracketCounter);
             addItem.DeleteBracket("Locking_Pin", GlobalVariables.bracketCounter);
@@ -104,6 +113,7 @@ namespace BRAC_FORM
 
             button4.Enabled = false;
             button3.Enabled = true;
+            button6.Enabled = false;
         }
 
         private void button1_Click(object sender, EventArgs e) ///////////////////// PREVIOUS
@@ -305,6 +315,79 @@ namespace BRAC_FORM
 
             dimBuilder.Commit();
             dimBuilder.Destroy();
+        }
+
+        private void button6_Click(object sender, EventArgs e) ////////////////////////// UPDATE
+        {
+            if (textBox1.Text != originalTextbox1 || textBox2.Text != originalTextbox2 || textBox3.Text != originalTextbox3 || textBox3.Text != originalTextbox3)
+            {
+                Class_Add_item addItem = new Class_Add_item();
+                addItem.DeleteBracket("Locking_brack", GlobalVariables.bracketCounter);
+                addItem.DeleteBracket("Locking_Pin", GlobalVariables.bracketCounter);
+                addItem.DeleteBracket("Lower_brac_new_m16", GlobalVariables.bracketCounter);
+                addItem.DeleteBracket("M6_35_NEW_BRAC", GlobalVariables.bracketCounter);
+                addItem.DeleteBracket("RPD_PIN", GlobalVariables.bracketCounter);
+                addItem.DeleteBracket("SAT_FUNK", GlobalVariables.bracketCounter);
+                addItem.DeleteBracket("Upper_NEW_clamp", GlobalVariables.bracketCounter);
+
+                Session theSession = Session.GetSession();
+                Part assemblyPart = (Part)theSession.Parts.Work;
+
+                GlobalVariables.bracketCounter++;
+
+                string D_width = GlobalVariables.PipeDiameter;
+                GlobalVariables.Width = textBox1.Text;
+                string Width = GlobalVariables.Width;
+
+                string XPos = "10";
+                string YPos = "10";
+
+                int IntPoint = (int)GlobalVariables.barrelEnd;
+
+                GlobalVariables.BracketPos = textBox2.Text;
+                int bracketPos = int.Parse(GlobalVariables.BracketPos);
+
+                double Pos = IntPoint + bracketPos;
+
+                int Forkint = (int)GlobalVariables.forkdistance + 1;
+                //string Gaffel_W = Forkint.ToString();
+
+                int bracketWidth = int.Parse(Width);
+
+                int Frontint = (int)GlobalVariables.forkpoint1[1]; // FrontPos
+                int FrontDif = Math.Abs(IntPoint - Frontint);
+                int FrontPOS = bracketPos - bracketWidth / 2 - FrontDif + 9;
+
+
+                //string Gaffel_L = FrontPOS.ToString();
+                string Gaffel_L = textBox3.Text;
+                string Gaffel_W = textBox4.Text;
+
+
+                //GlobalVariables.FilePath = GlobalVariables.FilePath + "\\Nya_CAD";
+                Point3d position = new Point3d(0.0, Pos, 0.0);
+                string partsFolderPath = GlobalVariables.FilePath;
+
+                addItem.AddPartToAssembly("Locking_brack.prt", GlobalVariables.bracketCounter, D_width + "," + Width, position, partsFolderPath, assemblyPart);
+                addItem.AddPartToAssembly("Locking_Pin.prt", GlobalVariables.bracketCounter, D_width, position, partsFolderPath, assemblyPart);
+                addItem.AddPartToAssembly("Lower_brac_new_m16.prt", GlobalVariables.bracketCounter, D_width + "," + Width, position, partsFolderPath, assemblyPart);
+                addItem.AddPartToAssembly("M6_35_NEW_BRAC.prt", GlobalVariables.bracketCounter, D_width, position, partsFolderPath, assemblyPart);
+                addItem.AddPartToAssembly("RPD_PIN.prt", GlobalVariables.bracketCounter, D_width + "," + Width, position, partsFolderPath, assemblyPart);
+                addItem.AddPartToAssembly("SAT_FUNK.prt", GlobalVariables.bracketCounter, D_width, position, partsFolderPath, assemblyPart);
+                addItem.AddPartToAssembly("Upper_NEW_clamp.prt", GlobalVariables.bracketCounter, D_width + "," + Width + "," + XPos + "," + YPos + "," + Gaffel_W + "," + Gaffel_L, position, partsFolderPath, assemblyPart);
+
+
+                addItem.updateAll();
+                addItem.HideDatumsAndSketches();
+
+                UI.GetUI().NXMessageBox.Show("Success", NXMessageBox.DialogType.Information, $"Bracket updated");
+                
+                button6.Enabled = true;
+            }
+            else 
+            {
+                UI.GetUI().NXMessageBox.Show("Error", NXMessageBox.DialogType.Information, $"Please change any parameter.");
+            }
         }
     }
 }
